f456db73dcf8a45233a48828ce690507739b9628
Luc Bourlier <luc.bourlier@typesafe.com>
2015-07-10 15:44:30 +0200

    Adds support for multi streams in log processor

--- log-processor/src/main/scala/com/typesafe/spark/testbed/logprocessor/ReceiverLogData.scala
+++ log-processor/src/main/scala/com/typesafe/spark/testbed/logprocessor/ReceiverLogData.scala
@@ -2,13 +2,13 @@ package com.typesafe.spark.testbed.logprocessor
 
 import java.text.SimpleDateFormat
 
-case class FeedbackLogData(time: Long, limit: Int) extends LogData[FeedbackLogData] {
+case class FeedbackLogData(time: Long, streamId: Int, limit: Int) extends LogData[FeedbackLogData] {
   def toCSVRow: String = s"$time $limit"
   
     def timeShift(shift: Long) = copy(time = time - shift)
 }
 
-case class RatioLogData(time: Long, ratio: Double) extends LogData[RatioLogData] {
+case class RatioLogData(time: Long, streamId: Int, ratio: Double) extends LogData[RatioLogData] {
   def toCSVRow: String = s"$time $ratio"
   
     def timeShift(shift: Long) = copy(time = time - shift)
@@ -18,20 +18,20 @@ object ReceiverLogData {
   
   private val dateParser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSSZ")
   
-  private val feedbackRegex = "([^ ]* [^ ]*).*update for.*: (\\d*)".r
-  private val ratioRegex = "([^ ]* [^ ]*).*with ratio of ([\\d.]*)\\.".r
+  private val feedbackRegex = "([^ ]* [^ ]*).*update for (\\d*) at.*: (\\d*)".r
+  private val ratioRegex = "([^ ]* [^ ]*).*stream (\\d*).*with ratio of ([\\d.]*)\\.".r
   
   def parseFeedback(line: String): FeedbackLogData = {
     line match {
-      case feedbackRegex(date, limit) =>
-        FeedbackLogData(dateParser.parse(date).getTime, limit.toInt)
+      case feedbackRegex(date, streamId, limit) =>
+        FeedbackLogData(dateParser.parse(date).getTime, streamId.toInt, limit.toInt)
     }
   }
   
   def parseRatio(line: String): RatioLogData = {
     line match {
-      case ratioRegex(date, ratio) =>
-        RatioLogData(dateParser.parse(date).getTime, ratio.toDouble)
+      case ratioRegex(date, streamId, ratio) =>
+        RatioLogData(dateParser.parse(date).getTime, streamId.toInt, ratio.toDouble)
     }
   }
 
